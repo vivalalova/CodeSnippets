e8f52bdad9d3419361085e0e7ef9659509279362
Lova <vivalalova0@gmail.com>
2016-12-13 05:34:53 +0800

    update

--- setup_snippets.sh
+++ setup_snippets.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
+rm ~/Library/Developer/Xcode/UserData/CodeSnippets
 
-# rm ~/Library/Developer/Xcode/UserData/CodeSnippets
-
-SRC_HOME=`pwd`
-ln -s ../CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+# ln -s ./ ~/Library/Developer/Xcode/UserData/CodeSnippets
+cp -r ./ ~/Library/Developer/Xcode/UserData/CodeSnippets
+open ~/Library/Developer/Xcode/UserData/
 echo "done"
 
